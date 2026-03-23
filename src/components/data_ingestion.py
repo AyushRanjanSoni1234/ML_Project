a3846cb9cf9d500440cec1db04d44819a86aa0b5
AyushRanjanSoni1234 <ars7408984015@gmail.com>
2026-03-23 17:23:12 +0530

    add artifact

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,9 @@
 import os
 import sys
+
+#  ✅ Fix: Add root directory to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.exception import CustomException
 from src.logger import logging
 import numpy as np
@@ -21,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv(os.path.join('notebook/data', 'gemstone.csv'))
+            df = pd.read_csv(os.path.join('notebook/data', 'stud.csv'))
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
