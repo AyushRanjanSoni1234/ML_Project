b5decfb71f7294a5ecc58882a1cf4ddca4604275
AyushRanjanSoni1234 <ars7408984015@gmail.com>
2026-03-26 19:51:27 +0530

    Add Model.pkl

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,8 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -20,3 +22,25 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        model_report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            model_report[list(models.keys())[i]] = test_score
+
+        return model_report
+    except Exception as e:
+        raise CustomException(e, sys)
+
